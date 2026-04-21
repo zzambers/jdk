fed0f5582b7b45f5e6ce61b164d3316ccb99c701
Zdenek Zambersky <zzambers@redhat.com>
2026-04-21 18:55:17 +0200

    OpensslArtifactFetcher: Use full path for system openssl

--- test/lib/jdk/test/lib/security/OpensslArtifactFetcher.java
+++ test/lib/jdk/test/lib/security/OpensslArtifactFetcher.java
@@ -96,8 +96,8 @@ public class OpensslArtifactFetcher {
     }
 
     private static String getDefaultSystemOpensslPath(String version) {
-        if (verifyOpensslVersion("openssl", version)) {
-            return "openssl";
+        if (verifyOpensslVersion("/usr/bin/openssl", version)) {
+            return "/usr/bin/openssl";
         } else if (verifyOpensslVersion("/usr/local/bin/openssl", version)) {
             return "/usr/local/bin/openssl";
         }
